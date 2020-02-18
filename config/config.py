5fb37ece43c70d9c8a92470f79e2b2420d4ceaed
luo <1249954696@qq.com>
2020-02-19 01:54:15 +0800

    [experiment] baseline_2

--- config/config.py
+++ config/config.py
@@ -9,19 +9,19 @@ cfg                             = __C
 __C.CLS                         = edict()
 __C.CLS.CLASSES                 = "./dataset/name.names"
 __C.CLS.DATASET_ROOT_DIR        = "../datasets/baseline_food/"
-__C.CLS.GPU                     = "0"
+__C.CLS.GPU                     = "3,2"
 
 # train options
 __C.TRAIN                       = edict()
 __C.TRAIN.FILE_PATH             = "./dataset/train.txt"
 __C.TRAIN.RESNET_SIZE           = 50
-__C.TRAIN.SAVE_DIR              = "../models/food_classification/baseline_1"
+__C.TRAIN.SAVE_DIR              = "../models/food_classification/baseline_2"
 __C.TRAIN.INPUT_SIZE            = 224
-__C.TRAIN.BATCH_SIZE            = 16
+__C.TRAIN.BATCH_SIZE            = 32
 __C.TRAIN.DATA_AUG              = True
 __C.TRAIN.LEARNING_RATE         = 1e-3
 __C.TRAIN.MOVING_AVE_DECAY      = 0.9
-__C.TRAIN.INITIAL_WEIGHT        = "../models/food_classification/baseline_1"
+__C.TRAIN.INITIAL_WEIGHT        = "../models/food_classification/baseline_2"
 __C.TRAIN.TOTAL_EPOCHS          = 50
 
 # test options
@@ -29,7 +29,7 @@ __C.TEST                        = edict()
 __C.TEST.FILE_PATH              = "./dataset/test.txt"
 __C.TEST.INPUT_SIZE             = 224
 __C.TEST.GPU                    = "1"
-__C.TEST.BATCH_SIZE             = 16
+__C.TEST.BATCH_SIZE             = 32
 __C.TEST.DATA_AUG               = False
 __C.TEST.MODEL_ZOO              = "../model_zoo/food_classification/baseline"
 __C.TEST.INITIAL_WEIGHT         = "../model_zoo/food_classification/baseline/resnet50_test_loss=2.8496.ckpt-15"
